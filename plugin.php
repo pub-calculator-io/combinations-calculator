ebc3eb615b3327e56d9fa818f2b65fcd532df47a
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 15:58:00 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_combinations_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Combinations Calculator by Calculator.iO";
 
 function display_ci_combinations_calculator(){
